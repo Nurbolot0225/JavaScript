ee5f7468677d22d4b16a7865b97efc36e4376358
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-19 00:30:23 +0600

    variables and operators (#4)

---
+++ app.js
@@ -0,0 +1,38 @@
+// Переменный
+
+// let a = 1;
+// console.log(a);
+// a = 'test';
+// console.log(a);
+
+// // Базовые арифметические операторы
+
+// const width = 10;
+// const height = 5;
+// const space = width * height;
+// const newWidth = width - 4;
+// const newHeight = height + 4;
+// const division = newWidth / newHeight;
+// const volume = 2 ** 3; CH// 2 * 2 * 2
+// console.log(volume);
+
+// Строки
+// const city = 'Москва';
+// const street = 'Новослободская';
+// console.log(city + ', ' + street);
+
+// Операторы присваивания
+// let age = 20 + 1;
+// age += 2; // age = age + 2
+// age -= 2; // age = age + 2
+// age *= 2; // age = age + 2
+// age /= 2; // age = age + 2
+// console.log(age);
+
+// Операторы сравнения
+// // const current Age = 20;
+// // console.log(current Age > age);
+// // console.log(current Age >= age);
+// // console.log(current Age < age);
+// // console.log(current Age <= age);
+// // console.log(current Age == age);
