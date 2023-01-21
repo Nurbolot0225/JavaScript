3885436a5b771de64839dab197c60c8a9d015429
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-21 13:51:13 +0600

    Data Types

--- app.js
+++ app.js
@@ -49,3 +49,22 @@
 // c = b = 100 + 50;
 // console.log(c);
 // console.log(b);
+
+// Типы данных
+let a = 5;
+let b = 5.6;
+console.log(typeof a);
+console.log(typeof b);
+a = 'строка';
+console.log(typeof a);
+
+let isAdmin = a > 10;
+console.log(typeof isAdmin);
+
+let c = undefined;
+console.log(typeof c);
+c = 5;
+console.log(typeof c);
+
+let d = null;
+console.log(typeof (d == null));
