9d87989690ff3cfd0a8b989ee2b4b65e0b506d01
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-21 14:21:53 +0600

    Template strings

--- app.js
+++ app.js
@@ -51,20 +51,38 @@
 // console.log(b);
 
 // Типы данных
-let a = 5;
-let b = 5.6;
-console.log(typeof a);
-console.log(typeof b);
-a = 'строка';
-console.log(typeof a);
+// let a = 5;
+// let b = 5.6;
+// console.log(typeof a);
+// console.log(typeof b);
+// a = 'строка';
+// console.log(typeof a);
+//
+// let isAdmin = a > 10;
+// console.log(typeof isAdmin);
+//
+// let c = undefined;
+// console.log(typeof c);
+// c = 5;
+// console.log(typeof c);
+//
+// let d = null;
+// console.log(typeof (d == null));
 
-let isAdmin = a > 10;
-console.log(typeof isAdmin);
-
-let c = undefined;
-console.log(typeof c);
-c = 5;
-console.log(typeof c);
-
-let d = null;
-console.log(typeof (d == null));
+// Шаблонные строки
+// const projectName = 'Сайт магазина';
+// const price = 2000;
+// const author = 'Василий';
+//
+// const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+// console.log(template);
+//
+// const template2 = `${author} 'заказал' ${projectName} по цене ${price}$`;
+// console.log(template2);
+//
+// const template3 = 'Проект \n' + 'Цена: ' + price + '$';
+// console.log(template3);
+//
+// const template4 = `Проект
+// Цена ${price}`;
+// console.log(template4)
