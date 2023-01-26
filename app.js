9330795a0b1c73f45847f98334c6dbc4cf2f96ef
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-26 22:51:41 +0600

    forEach - iterating the array(11)

--- app.js
+++ app.js
@@ -531,5 +531,15 @@
 //
 // console.log(power(5)(4));
 
-const power = pow => num => num**pow;
-console.log(power(2)(3));
+// const power = pow => num => num**pow;
+// console.log(power(2)(3));
+
+//////////// forEach ///////////////
+const score = [5, 10, 0, 15];
+for (const [i, el] of score.entries()) {
+    console.log(`Раунд: ${i + 1}: ${el}`);
+}
+
+score.forEach((el, i) => {
+    console.log(`Раунд: ${i + 1}: ${el}`);
+});
