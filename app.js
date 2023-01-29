e458baafa5b7afa74e586ad9ec00acb246b18401
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-01-29 23:42:14 +0600

    Quick creation of arrays(11)

--- app.js
+++ app.js
@@ -655,25 +655,34 @@
 
 
 ///////////////// sort /////////////////////////
-const users = ['Маша', 'Вася', 'Катя', 'Аня'];
-console.log(users);
-users.sort()
-console.log(users);
+// const users = ['Маша', 'Вася', 'Катя', 'Аня'];
+// console.log(users);
+// users.sort()
+// console.log(users);
+//
+// const operations = [100, -300, -100, 50, 480];
+// console.log(operations);
+// // < 0 - a, b - сохраняем порядок
+// // > 0 - b, a - меняем порядок
+// operations.sort((a,b) => a - b);
+// console.log(operations);
+//
+// operations.sort((a,b) => {
+//     if (a < b) {
+//         return 1;
+//     }
+//     if (a > b) {
+//         return -1
+//     }
+// })
+// console.log(operations);
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);
-// < 0 - a, b - сохраняем порядок
-// > 0 - b, a - меняем порядок
-operations.sort((a,b) => a - b);
-console.log(operations);
+//////////////// Быстрое создание массив ////////////////
+const arr = [1, 2, 3, 4, 5];
+console.log(new Array(1, 2, 3, 4, 5));
+const arr2 = new Array(5);
+console.log(arr2.fill(1, 0, 3));
+console.log(arr2.fill(2, 3, 5));
 
-operations.sort((a,b) => {
-    if (a < b) {
-        return 1;
-    }
-    if (a > b) {
-        return -1
-    }
-})
-console.log(operations);
-
+const arr3 = Array.from({ length: 5}, (cur, i) => i + 1);
+console.log(arr3);
