3a1b057822dc1e85588921fdab529082a577a3f8
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-05 00:27:34 +0600

    Exercise - Warehouse(13)

--- app.js
+++ app.js
@@ -959,19 +959,75 @@
 // }
 // console.log(user);
 
-const cities = {
-    msk: {
-        temp: {
-            celsius: 25
-        }
+////////////// Optional chaining ////////////////
+// const cities = {
+//     msk: {
+//         temp: {
+//             celsius: 25
+//         }
+//     },
+//     spb: {
+//
+//     }
+// }
+// const city = 'msk'
+// if (cities[city] !== undefined && cities[city].temp !== undefined) {
+//     console.log(cities[city].temp.celsius);
+// }
+// console.log(cities[city]?.temp?.celsius);
+
+/*
+    Сделать объект склад с методами добавления на склад,
+    поиска по складу товара и расчёт веса
+*/
+const warehouse = {
+    goods: [],
+    findGoodById: function (id) {
+        return this.goods.find(g => g.id === id);
     },
-    spb: {
-
+    addGood: function (good) {
+        const existedGood = this.findGoodById(good.id);
+        if (existedGood) {
+            console.log('Этот товар уже есть на складе');
+            return;
+        }
+        this.goods.push(good);
+    },
+    getWeightKg: function () {
+        return this.goods.reduce((acc, el) =>
+            acc += el.weight?.kg ? el.weight.kg : 0,
+            0
+        )
     }
-}
-const city = 'msk'
-if (cities[city] !== undefined && cities[city].temp !== undefined) {
-    console.log(cities[city].temp.celsius);
-}
+};
 
-console.log(cities[city]?.temp?.celsius);
+/* товары */
+const car = {
+    id: 1,
+    weight: {
+        kg: 1000
+    },
+    brand: 'Ford'
+};
+const chair = {
+    id: 1,
+    weight: {
+        kg: 2
+    },
+};
+const paper = {
+    id: 3,
+    color: 'red'
+};
+
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
+let findedItem = warehouse.findGoodById(6);
+console.log(findedItem);
+findedItem = warehouse.findGoodById(1);
+console.log(findedItem);
+const w = warehouse.getWeightKg();
+console.log(w);
