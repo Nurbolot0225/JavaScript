fb0d0c83e042a0ca3dc8e9ae0487903f899a2d84
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-06 17:52:46 +0600

    bind(16)

--- app.js
+++ app.js
@@ -1174,35 +1174,59 @@
 // }
 // console.log(a);
 
+//////////////// call apply ///////////////////////
+// const audi = {
+//     make: 'Audi',
+//     model: 'A3',
+//     year: 2021,
+//     damages: [],
+//     addDamage(part, rate) {
+//         console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`)
+//         this.damages.push({
+//             part,
+//             rate
+//         });
+//     }
+// }
+// audi.addDamage('Капот', 1);
+//
+// const bmw = {
+//     name: 'BMW',
+//     model: 'X5',
+//     year: 2022,
+//     damages: [],
+// }
+//
+// bmw.addDamage = audi.addDamage;
+// // bmw.addDamage('Бампер', 2);
+
+// const addDamageFunc = audi.addDamage;
+// // addDamageFunc('Бампер', 2);
+// addDamageFunc.call(bmw, 'Бампер', 2);
+// addDamageFunc.call(audi, 'Бампер', 2);
+
+// addDamageFunc.apply(bmw, ['Юбка', 2]);
+// addDamageFunc.apply(audi, ['Юбка', 2]);
+
+////////////////////  bind  //////////////////////
 const audi = {
     make: 'Audi',
     model: 'A3',
-    year: 2021,
-    damages: [],
+    damages: []
+};
+
+const carManipulation = {
     addDamage(part, rate) {
-        console.log(`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`)
-        this.damages.push({
-            part,
-            rate
-        });
+        this.damages.push({ part, rate });
+        console.log(`Добавить повреждение на ${this.make} ${this.model}`);
     }
 }
-audi.addDamage('Капот', 1);
 
-const bmw = {
-    name: 'BMW',
-    model: 'X5',
-    year: 2022,
-    damages: [],
-}
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Крыло', 3);
 
-bmw.addDamage = audi.addDamage;
-// bmw.addDamage('Бампер', 2);
+const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
+addDamageAudiRoof(5);
+addDamageAudiRoof(4);
+console.log(audi);
 
-const addDamageFunc = audi.addDamage;
-// addDamageFunc('Бампер', 2);
-addDamageFunc.call(bmw, 'Бампер', 2);
-addDamageFunc.call(audi, 'Бампер', 2);
-
-addDamageFunc.apply(bmw, ['Юбка', 2]);
-addDamageFunc.apply(audi, ['Юбка', 2]);
