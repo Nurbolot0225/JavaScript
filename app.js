25bb5e8d086028b6f2f7f61b1a3b675f15765515
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-07 19:02:03 +0600

    DOCUMENT API(17)

--- app.js
+++ app.js
@@ -1282,38 +1282,45 @@
 // change2(200);
 
 ///////////////////////// Упражнение - работа с замыканиями /////////////////////////////
-const userInfo = {
-    balance: 0,
-    operations: 0,
-    increase(sum) {
-        this.balance += sum;
-        this.operations++;
-    }
-}
+// const userInfo = {
+//     balance: 0,
+//     operations: 0,
+//     increase(sum) {
+//         this.balance += sum;
+//         this.operations++;
+//     }
+// }
+//
+// function user() {
+//     const userObj = {
+//         balance: 0,
+//         operations: 0,
+//         increase(sum) {
+//             this.balance += sum;
+//             this.operations++;
+//         }
+//     };
+//     return function() {
+//         return userObj
+//     }
+// }
+//
+// const user1 = user();
+// user1().increase(100);
+// user1().increase(100);
+// console.log(user1());
+//
+// const user2 = user();
+// user2().increase(100);
+// console.log(user2());
+//
+// const user3 = user();
+// user3().increase(100);
+// console.log(user3());
 
-function user() {
-    const userObj = {
-        balance: 0,
-        operations: 0,
-        increase(sum) {
-            this.balance += sum;
-            this.operations++;
-        }
-    };
-    return function() {
-        return userObj
-    }
-}
+////////////////////////////////////////     DOCUMENT      /////////////////////////////////////////
 
-const user1 = user();
-user1().increase(100);
-user1().increase(100);
-console.log(user1());
-
-const user2 = user();
-user2().increase(100);
-console.log(user2());
-
-const user3 = user();
-user3().increase(100);
-console.log(user3());
+const panelText = document.querySelector('.panel').innerText;
+console.log(panelText);
+document.querySelector('.panel').innerText = 'New Text';
+document.querySelector('.input').value = 'Text';
