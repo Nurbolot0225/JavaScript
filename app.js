09262011cb0e1f0eb3e14df8c88e430b80a5e414
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-07 10:40:43 +0600

    Exercise - Managing this(16)

--- app.js
+++ app.js
@@ -1209,24 +1209,40 @@
 // addDamageFunc.apply(audi, ['Юбка', 2]);
 
 ////////////////////  bind  //////////////////////
-const audi = {
-    make: 'Audi',
-    model: 'A3',
-    damages: []
-};
+// const audi = {
+//     make: 'Audi',
+//     model: 'A3',
+//     damages: []
+// };
+//
+// const carManipulation = {
+//     addDamage(part, rate) {
+//         this.damages.push({ part, rate });
+//         console.log(`Добавить повреждение на ${this.make} ${this.model}`);
+//     }
+// }
+//
+// const addDamageAudi = carManipulation.addDamage.bind(audi);
+// addDamageAudi('Крыло', 3);
+//
+// const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
+// addDamageAudiRoof(5);
+// addDamageAudiRoof(4);
+// console.log(audi);
 
-const carManipulation = {
-    addDamage(part, rate) {
-        this.damages.push({ part, rate });
-        console.log(`Добавить повреждение на ${this.make} ${this.model}`);
+function removePassword(reset) {
+    if (reset) {
+        this.password = undefined;
+    } else {
+        this.password = '1';
     }
 }
 
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-addDamageAudi('Крыло', 3);
+const user = {
+    login: 'nur@a.ru',
+    password: '12345'
+};
 
-const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
-addDamageAudiRoof(5);
-addDamageAudiRoof(4);
-console.log(audi);
-
+const resetUserPassword = removePassword.bind(user, true);
+resetUserPassword();
+console.log(user);
