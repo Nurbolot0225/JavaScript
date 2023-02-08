160d4370d5ab8ef740b7a48fefc82f0ee4b7d718
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-08 12:29:55 +0600

    117 Обработка сÐKeyboard Event Handling(17)

--- app.js
+++ app.js
@@ -1334,11 +1334,18 @@
 //     document.querySelector('.input').value = '';
 // });
 
-function changeClick() {
+function submitForm() {
     const input = document.querySelector('.input').value;
     if(!input) {
         return;
     }
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
+}
+
+function inputChanged(e) {
+    if (e.code === 'Enter') {
+        submitForm();
+        console.log(e);
+    }
 }
