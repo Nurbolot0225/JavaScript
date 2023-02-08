39e11229ae379db28cdac329e4160b62813275e6
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-02-08 16:27:05 +0600

    Exercise - Getting DOM elements(17)

--- app.js
+++ app.js
@@ -1356,3 +1356,14 @@ function inputChanged(e) {
         console.log(e);
     }
 }
+
+// console.log(document.querySelector('.one').innerText);
+// console.log(document.querySelector('.one ~ div').innerText);
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+
+// console.log(document.querySelector('#two').innerText);
+console.log(document.getElementById('two').innerText);
+console.log(document.getElementsByClassName('one'))
+console.log(document.querySelector('[user-id="4"]').innerText);
+
